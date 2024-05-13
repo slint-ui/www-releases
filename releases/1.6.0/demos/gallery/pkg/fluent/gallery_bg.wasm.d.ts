65bd791ee4cf41c68990e0a27330ca0569ee727e
tronical <tronical@users.noreply.github.com>
2024-05-13 08:19:03 +0000

    Update  from slint-ui/slint
    Pull web demos and C++/Rust reference docs from commit 644e15dee19fb1a72975c81e62892b9251a1111a (refs/heads/master)

---
+++ releases/1.6.0/demos/gallery/pkg/fluent/gallery_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function slint_mock_elapsed_time(a: number): void;
+export function slint_get_mocked_time(): number;
+export function slint_send_mouse_click(a: number, b: number, c: number): void;
+export function slint_send_keyboard_char(a: number, b: number, c: number): void;
+export function send_keyboard_string_sequence(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2c9218b466a9ae63(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A_B___Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0ce68c30704109f5(a: number, b: number, c: number, d: number): void;
+export function _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h4f3628edf3cafcc3(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hbb827f0ee43216b0(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
---
+++ releases/1.6.0/demos/todo/pkg/todo_lib_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function slint_mock_elapsed_time(a: number): void;
+export function slint_get_mocked_time(): number;
+export function slint_send_mouse_click(a: number, b: number, c: number): void;
+export function slint_send_keyboard_char(a: number, b: number, c: number): void;
+export function send_keyboard_string_sequence(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7e76d01e7c222ff4(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A_B___Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h433604e4ecaf464b(a: number, b: number, c: number, d: number): void;
+export function _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h351fcf8c2844a1a8(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h352be82d88762ad3(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1c49456ba40fb387(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb41192369a3fa3d5(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
